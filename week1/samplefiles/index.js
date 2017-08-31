735c70fe687e91c12d8470f4f65f8b4b2e0ed76f
egunn <erica.gunn@gmail.com>
2017-08-30 21:14:54 -0400

    week1 files

---
+++ week1/samplefiles/index.js
@@ -0,0 +1,9 @@
+//a single line of comment in javascript looks like this
+
+/*multiple lines of
+ comments use the same
+ syntax as comments in CSS */
+
+/* this writes a message to the developer console when the script runs
+Other than that, we're going to ignore the .js file for this week. */
+console.log("here!");
